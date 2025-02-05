21c6aee6b34736f9ed6ff440dd2bce8d2f7ef55d
Pascal Ramy <pascal.ramy00@gmail.com>
2025-02-05 15:02:05 -0500

    add portfolio demo, finalize for first launch

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -15,6 +15,16 @@ import {
 
 export const projects: Project[] = [
 	{
+		title: "My Portfolio",
+		description: "Next.js Portfolio Site",
+		story: "This is a story. It all started when dr.peabrain drew me at my desk.",
+		features: ["animated", "dark mode", "responsive", "interactive"],
+		videoUri: "/videos/portfolio_site_demo_encoded.mp4",
+		stack: [NextJS, ReactJS, TypeScript, TailwindCSS, FramerMotion, Vercel],
+		date: new Date("January 2025"),
+		url: "https://pascalramy.com",
+	},
+	{
 		title: "New Milanes",
 		description: "Wordpress E-Commerce Site",
 		story: "Hired by CAPi, a digital creative agency, to build their design for New Milanes, an italian fashion brand.",
@@ -28,15 +38,5 @@ export const projects: Project[] = [
 		stack: [WordPress, WooCommerce, WPML, RankMath],
 		date: new Date("December 2024"),
 		url: "https://newmilanes.com",
-	},
-	{
-		title: "My Portfolio",
-		description: "Next.js Portfolio Site",
-		story: "This is a story. It all started when dr.peabrain drew me at my desk.",
-		features: ["animated", "dark mode", "responsive", "interactive"],
-		videoUri: "/videos/new_milanes_demo_encoded.mp4",
-		stack: [NextJS, ReactJS, TypeScript, TailwindCSS, FramerMotion, Vercel],
-		date: new Date("January 2025"),
-		url: "https://pascalramy.com",
 	},
 ];
