5de0cbb0fdea8469f38d8cb9ae0a864f236896c5
Pascal Ramy <pascalramy@Pascals-MacBook-Pro-2.local>
2025-02-19 09:18:14 -0500

    update cv link

--- src/components/ui/sections/Hero.tsx
+++ src/components/ui/sections/Hero.tsx
@@ -6,35 +6,31 @@ import Section from "@/components/layout/Section";
 import HeroOptimizedArtwork from "../elements/HeroOptimizedArtwork";
 
 const Hero = () => {
-	return (
-		<Section
-			id="home"
-			className="flex flex-col-reverse justify-center items-center  lg:flex-row lg:pt-20 lg:items-end lg:justify-start"
-		>
-			<div className="">
-				<h1 className="text-4xl 2xl:text-6xl mb-2 md:mb-14 2xl:mb-20">
-					<AnimatedGreeting />
-					<br /> I&apos;m{" "}
-					<span className="font-bold">Pascal Ramy</span>
-				</h1>
+  return (
+    <Section
+      id="home"
+      className="flex flex-col-reverse justify-center items-center  lg:flex-row lg:pt-20 lg:items-end lg:justify-start"
+    >
+      <div className="">
+        <h1 className="text-4xl 2xl:text-6xl mb-2 md:mb-14 2xl:mb-20">
+          <AnimatedGreeting />
+          <br /> I&apos;m <span className="font-bold">Pascal Ramy</span>
+        </h1>
 
-				<div className="">
-					<ExternalLink
-						href="https://www.linkedin.com/in/pascal-ramy/"
-						label="Linkedin"
-					/>
-					<ExternalLink
-						href="/docs/Pascal_Ramy_Software_Developer.pdf"
-						label="Resume"
-					/>
-				</div>
-				<h2 className="mb-4 2xl:text-xl">
-					Software Developer based in{" "}
-					<strong className="font-semibold">Montreal, Canada</strong>
-				</h2>
-			</div>
-			<HeroOptimizedArtwork />
-		</Section>
-	);
+        <div className="">
+          <ExternalLink
+            href="https://www.linkedin.com/in/pascal-ramy/"
+            label="Linkedin"
+          />
+          <ExternalLink href="/docs/PRamy_CV.pdf" label="Resume" />
+        </div>
+        <h2 className="mb-4 2xl:text-xl">
+          Software Developer based in{" "}
+          <strong className="font-semibold">Montreal, Canada</strong>
+        </h2>
+      </div>
+      <HeroOptimizedArtwork />
+    </Section>
+  );
 };
 export default Hero;
