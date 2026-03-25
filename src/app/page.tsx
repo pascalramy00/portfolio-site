fd8477d52ad07b14121f3164f3cf176011b9ce3a
Pascal Ramy <pascal.ramy00@gmail.com>
2026-03-25 19:11:54 -0400

    reorder sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,17 @@ import ScrollToTop2 from "@/components/ui/animations/ScrollToTop2";
 import ScrollProgressBar from "@/components/ui/animations/ScrollProgressBar";
 
 export default function Home() {
-	return (
-		<main className="p-4 pb-10 md:px-20 md:py-0 overflow-x-hidden">
-			<Hero />
-			<About />
-			<Stack />
-			<Projects />
-			<Contact />
-			<ChevronAnimation className="absolute bottom-0 left-[46%] lg:left-[50%]" />
-			<ScrollToTop2 />
-			<ScrollProgressBar />
-			<div className="hidden lg:block lg:w-1/6"></div>
-		</main>
-	);
+  return (
+    <main className="p-4 pb-10 md:px-20 md:py-0 overflow-x-hidden">
+      <Hero />
+      <Projects />
+      <Stack />
+      <Contact />
+      <About />
+      <ChevronAnimation className="absolute bottom-0 left-[46%] lg:left-[50%]" />
+      <ScrollToTop2 />
+      <ScrollProgressBar />
+      <div className="hidden lg:block lg:w-1/6"></div>
+    </main>
+  );
 }
--- src/data/navLinks.ts
+++ src/data/navLinks.ts
@@ -1,20 +1,20 @@
 import type { NavLink as NavLinkType } from "@/types";
 import {
-	HomeIcon,
-	CodeBracketIcon,
-	CircleStackIcon,
-	EnvelopeIcon,
-	UserIcon,
+  HomeIcon,
+  CodeBracketIcon,
+  CircleStackIcon,
+  EnvelopeIcon,
+  UserIcon,
 } from "@heroicons/react/24/solid";
 
 export const navLinks: NavLinkType[] = [
-	{
-		label: "Home",
-		href: "#home",
-		Icon: HomeIcon,
-	},
-	{ label: "About", href: "#about", Icon: UserIcon },
-	{ label: "Stack", href: "#stack", Icon: CircleStackIcon },
-	{ label: "Projects", href: "#projects", Icon: CodeBracketIcon },
-	{ label: "Contact", href: "#contact", Icon: EnvelopeIcon },
+  {
+    label: "Home",
+    href: "#home",
+    Icon: HomeIcon,
+  },
+  { label: "About", href: "#about", Icon: UserIcon },
+  { label: "Stack", href: "#stack", Icon: CircleStackIcon },
+  { label: "Projects", href: "#projects", Icon: CodeBracketIcon },
+  { label: "Contact", href: "#contact", Icon: EnvelopeIcon },
 ];
