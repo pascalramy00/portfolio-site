80544d1c83c373880f7e43325c3d1d813262620e
Pascal Ramy <pascal.ramy00@gmail.com>
2026-03-28 14:25:29 -0400

    update CV

--- src/data/externaLinks.ts
+++ src/data/externaLinks.ts
@@ -5,7 +5,7 @@ const externalLinks: ExternalLink[] = [
     label: "LinkedIn",
     href: "https://www.linkedin.com/in/pascal-ramy/",
   },
-  { label: "Resume", href: "/docs/PRamy_CV.pdf" },
+  { label: "Resume", href: "/docs/PRAMY_CV_2026_EN.pdf" },
 ];
 
 export default externalLinks;
